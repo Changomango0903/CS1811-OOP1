3eb5b1dfc98ffb79125880e731ba4c40b4961ddd
Chango <schang09032003@gmail.com>
2022-09-30 07:04:10 +0800

    Initial Commit: Finished Exercise Sheet 1

---
+++ Exercise/SheetOne/Lines.java
@@ -0,0 +1,7 @@
+public class Lines {
+	public static void main(String[] args) {
+		System.out.println("It's easy to learn Java.");
+		System.out.println("Write, compile, run...");
+		System.out.println("                   ...and enjoy!");
+	}
+}
---
+++ Exercise/SheetOne/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+	public static void main(String[] args) {
+		int x, y;
+		x = 4;
+		y = 5;
+		System.out.println(x + ", " + y);
+		System.out.println("x = " + x + ", y = " + y + ", x+y = " + (x+y));
+
+	}
+}

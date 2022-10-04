954254333d36d0390014ed9e85758a8a6ddf9b81
Chango <schang09032003@gmail.com>
2022-10-04 20:42:38 +0800

    Summative One Assessment Completed

---
+++ Summative Quiz/Summative One/myClassName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class myClassName {
+
+	public static void main(String[] args) {
+		Scanner key = new Scanner(System.in);
+		String name = key.nextLine();
+		System.out.println("Hello, " + name);
+	}
+}
